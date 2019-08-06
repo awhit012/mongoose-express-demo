6900e920ccaea7c3fec5e393dbe664e4dad4abb1
awhit012 <awhit012@gmail.com>
2019-08-06 09:16:29 -0600

    adds comments

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ db.once('open', () => {
 
 class Kittens {
   constructor() {
+    // mongoose gives us a Schema function allowing us to define our documents
     this.kittySchema = mongoose.Schema({
-    name: String
+      name: String
     })
+    // models get a capital letter by convention. 
+    // we create a model passing it a name and a Schema
+    // by convention the name passed in is the same as the variable name
     this.Kitten = mongoose.model('Kitten', this.kittySchema);
     this.allKittens = [];
     this.allKittens.push( new this.Kitten({ name: 'Silence' }));
